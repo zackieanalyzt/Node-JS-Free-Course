8cbf134c7fad78e916534129e73b8eda099107c2
zackieAnalyzt <analyztman@gmail.com>
2025-03-02 10:49:10 +0700

    add index.html
    add folder public
    add file index.html

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
+
 const app = express();
 const port = 3000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req, res) => {
     res.send('hello zackie hybrid');
